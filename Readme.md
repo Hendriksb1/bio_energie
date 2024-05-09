ffb3419fe052af95f3458168892b04b90df3af1e
hendriksb1 <hendrik.schulze.boeing@gmail.com>
2024-05-09 23:53:44 +0200

    added threat and schedul task

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 source /Users/hsb/dev/bio_energie/bin/activate
+
+url = "https://api.corrently.io/v2.0/gsi/marketdata?zip=59077"
--- getPrice.py
+++ getPrice.py
@@ -2,30 +2,38 @@ import json
 import requests
 import pandas as pd
 import datetime
+import threading
+import time
 import os
 from dotenv import load_dotenv
 
-
-# url = "https://api.corrently.io/v2.0/gsi/marketdata?zip=59077"
-
+# Function to schedule and execute the task
+def schedule_task(urlPrice: str, urlWeather: str):
+    while True:
+        df = getPriceFrame(urlPrice)
+        df2 = getWeatherFrame(urlWeather)
+         # Inner join on 'datetime' column
+        result = pd.merge(df, df2, on='date_time', how='inner')
+        print(result)
+        time.sleep(5)  # Sleep for 5 seconds before next execution
 
 def main():
-
     load_dotenv()
-    
+
+    # Get URLs from environment variables
     urlPrice = os.getenv("URL_PRICE")
     urlWeather = os.getenv("URL_WEATHER")
 
-    df = getPriceFrame(urlPrice)
-    df2 = getWeatherFrame(urlWeather)
+    # Create a thread for the scheduled task
+    task_thread = threading.Thread(target=schedule_task(urlPrice, urlWeather))
 
-    # Inner join on 'datetime' column
-    result = pd.merge(df, df2, on='date_time', how='inner')
-    print(result)
+    # Start the thread
+    task_thread.start()
 
-
+# Function to get weather data and return as DataFrame
 def getWeatherFrame(url) -> pd.DataFrame:
     try:
+        # Send GET request to weather API
         res = requests.get(url)
 
         if res.status_code != 200 :
@@ -40,6 +48,7 @@ def getWeatherFrame(url) -> pd.DataFrame:
     finally:
         return out
 
+# Function to get price data and return as DataFrame
 def getPriceFrame(url) -> pd.DataFrame:
     try:
         res = requests.get(url)
@@ -64,10 +73,13 @@ def milliseconds_to_datetime(ms):
     dt_object = datetime.datetime.fromtimestamp(timestamp_sec)
     return dt_object
 
+# Function to convert seconds since 1970 to datetime objects
 def seconds_to_datetime(ms):
     # Convert the timestamp to a datetime object
     return datetime.datetime.fromtimestamp(ms)
 
+
+# Function to handle price API response and return DataFrame
 def handle_price_response(res: str) -> pd.DataFrame:
 
     data = json.loads(res)["data"]
@@ -81,7 +93,7 @@ def handle_price_response(res: str) -> pd.DataFrame:
 
     return df[['date_time', 'marketprice', 'unit']]
     
-
+# Function to handle weather API response and return DataFrame
 def handle_weather_response(res: str) -> pd.DataFrame:
 
     data = json.loads(res)["hourly"]["data"]
@@ -99,8 +111,6 @@ def handle_weather_response(res: str) -> pd.DataFrame:
 
     # at night there is no sun 
     df.loc[df['icon'] == "clear-night", 'sun_index'] = 0
-
-    print(df)
 
     return df[['date_time', 'sun_index']]
 
